4caec10e55b4b0dd213b95cda1fe2102c1115cb0
amisha2024-iist <ami2004namdev@gmail.com>
2026-04-18 19:50:16 +0530

    Create extend_list_operation.py

---
+++ Lists in python /extend_list_operation.py
@@ -0,0 +1,9 @@
+#extend operation:adds an element at the end of a list
+#appends function also do the same but it can add only one element but extend function can add  more then one element in the end of the list.
+fruits=["mango","apple","banana"]
+fruits.extend(["orange","pineapple","peach"])
+print(fruits)
+
+ceos=["satya nadela","sundar pichai"]
+ceos.extend(["arvind krishna","tim cook"])
+print(ceos)
