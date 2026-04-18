f688ebc75286a848f9508bc3710b359e37b35da1
amisha2024-iist <ami2004namdev@gmail.com>
2026-04-18 19:09:49 +0530

    Create list_python.py to demonstrate list operations
    Added a Python script to demonstrate list usage, including indexing and length.

---
+++ Lists in python /list_python.py
@@ -0,0 +1,17 @@
+student=["john",20,85.5]
+print(type(student))
+print(student)
+#              -7    -6     -5    -4    -3      -2   -1
+days_of_week =["mon","tue","wed","thu","fri","sat","sun"]
+             # 0       1    2      3     4      5    6
+print(days_of_week)
+print(days_of_week[0])
+print(days_of_week[1])
+print(days_of_week[-4])
+print(days_of_week[-3])
+print(days_of_week[-2])
+print(f"last day of the wek is {days_of_week[-1]}")
+
+#lengths of the list = number of items/elements in the list
+print(len(days_of_week))
+#if we print(days_of_week[8]) then it will give an error(out of range) because it is greater than the length of the list.
