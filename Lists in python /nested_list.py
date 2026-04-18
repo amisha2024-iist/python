d145e50ab5a404ce26bd72fe0a718d4b0b1d83a3
amisha2024-iist <ami2004namdev@gmail.com>
2026-04-19 00:00:54 +0530

    Create nested_list.py

---
+++ Lists in python /nested_list.py
@@ -0,0 +1,9 @@
+#list inside a list
+"""l1=[5,2.4,"python",True,None,[1,2,3],10]
+#print(len(l1))
+print(l1[-2])
+print(l1[-2][2])"""
+
+l2=[[1,2],[3,4],[5,6,[0,1]]]
+print(len(l2))
+print(l2[-1][-1][-1])
