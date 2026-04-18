a0da08f80387ee4099c3aa41f7bd3a80793ab7e1
amisha2024-iist <ami2004namdev@gmail.com>
2026-04-18 18:42:22 +0530

    Create count_operation.py

---
+++ Python practice/count_operation.py
@@ -0,0 +1,22 @@
+#counting substring from a string
+#count()
+#string.count(substring)
+"""s1="my day was good today"
+s2="o"
+print(f"occurrence of {s1} is {s1.count(s2)}") #o occur 3 times in s1
+"""
+#changing case of a string
+#upper(),lower(),title(),capitalize()
+s1="my day was good today"
+"""print(s1.upper())
+print(s1.lower())
+print(s1.capitalize())
+print(s1.title())
+"""
+
+#starting and ending of an string
+#startswith():
+#string.startswith(substring)
+print(s1.startswith("my"))
+#endswith
+print(s1.endswith("day"))
