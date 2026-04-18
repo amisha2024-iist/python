e7394d2634170722d10b865093b9c6a361ef2fb7
amisha2024-iist <ami2004namdev@gmail.com>
2026-04-18 23:49:21 +0530

    Create num_operation_element.py

---
+++ Lists in python /num_operation_element.py
@@ -0,0 +1,10 @@
+numbers=[10,4,5.5,7,1]
+
+#smallest and greatest number in the list
+#min()
+#max
+print(f"smallest number: {min(numbers)}")
+print(f"biggest number: {max(numbers)}")
+#total of the numbers in the list
+#sum
+print(f"total:{sum(numbers)}")
