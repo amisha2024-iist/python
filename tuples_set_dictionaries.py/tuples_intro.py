61c541aa7d9d20bd1fe938ac53b468145ba0721c
amisha2024-iist <ami2004namdev@gmail.com>
2026-04-19 16:15:41 +0530

    Create tuples_intro.py

---
+++ tuples_set_dictionaries.py/tuples_intro.py
@@ -0,0 +1,18 @@
+#Tuple:comma separated elements enclosed within brackets
+#(item1,item2,item3,........)
+#sequence of items as a collection
+#in tuple we don't have such operations like list
+#we can't append,extend,remove,pop,reverse or any list operation in tuple
+#Tuples is fixed we can't make change in it.
+t1=("python",10,1.5,True,[1,3,4],(10,20))
+print(t1)
+print(len(t1))
+
+#accessing items of a tuple - index
+print(t1[0])
+print(t1[-1])
+t1=(10,20,40)
+t2=30,40,45 #tuples can also be wright without round brackets.
+print(t1)
+print(type(t1))
+print(type(t2))
